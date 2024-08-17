0af4797c9afc88f45abce211ae37ef5dc98eb3fe
mohammad sarwary <mshazemsarwary@gamil.com>
2024-08-17 16:34:00 -0400

    add java scrip to fitch and display movies from API

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const API_URL = '<https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1>';
+
+const main = document.getElementById('main');
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img src="<https://image.tmdb.org/t/p/w1280${movie.poster_path}>" alt="${movie.title}">`;
+        main.appendChild(movieEl);
+    });
+}
+
+getMovies(API_URL);
